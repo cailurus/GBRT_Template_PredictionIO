957aea73edd2b5fc40336f47adc91d6bba60d62b
ailurus1991 <pandachow19910411@gmail.com>
2015-11-26 02:50:54 -0800

    first blood

---
+++ src/main/scala/Engine.scala
@@ -0,0 +1,29 @@
+package org.template.classification
+
+import io.prediction.controller.EngineFactory
+import io.prediction.controller.Engine
+
+class Query(
+  val attr0 : Double,
+  val attr1 : Double,
+  val attr2 : Double
+) extends Serializable
+
+class PredictedResult(
+  val label: Double
+) extends Serializable
+
+class ActualResult(
+  val label: Double
+) extends Serializable
+
+object ClassificationEngine extends EngineFactory {
+  def apply() = {
+    new Engine(
+      classOf[DataSource],
+      classOf[Preparator],
+      Map("naive" -> classOf[NaiveBayesAlgorithm],
+          "randomforest" -> classOf[RandomForestAlgorithm]),
+      classOf[Serving])
+  }
+}
---
+++ src/main/scala/RandomForestAlgorithm.scala
@@ -0,0 +1,80 @@
+package org.template.classification
+
+import io.prediction.controller.P2LAlgorithm
+import io.prediction.controller.Params
+
+//import org.apache.spark.mllib.classification.NaiveBayes
+//import org.apache.spark.mllib.classification.NaiveBayesModel
+import org.apache.spark.mllib.tree.RandomForest
+import org.apache.spark.mllib.tree.model.RandomForestModel
+
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.SparkContext
+
+import grizzled.slf4j.Logger
+
+/*
+case class AlgorithmParams(
+  lambda: Double
+) extends Params
+*/
+case class RandomForestAlgorithmParams(
+    numClasses: Int,
+    numTrees: Int,
+    featureSubsetStrategy: String,
+    impurity: String,
+    maxDepth: Int,
+    maxBins: Int
+) extends Params
+
+class RandomForestAlgorithm(val ap: RandomForestAlgorithmParams)
+    extends P2LAlgorithm[PreparedData, RandomForestModel, Query, PredictedResult] {
+
+    def train(sc: SparkContext, data: PreparedData): RandomForestModel = {
+        val categoricalFeatureInfo = Map[Int, Int]()
+        RandomForest.trainClassifier(
+            data.labeledPoints,
+            ap.numClasses,
+            categoricalFeatureInfo,
+            ap.numTrees,
+            ap.featureSubsetStrategy,
+            ap.impurity,
+            ap.maxDepth,
+            ap.maxBins)
+    }
+
+    def predict(model: RandomForestModel, query: Query): PredictedResult = {
+        val label = model.predict(Vectors.dense(
+            query.attr0, query.attr1, query.attr2
+            ))
+        new PredictedResult(label)
+    }
+}
+
+/*
+
+// extends P2LAlgorithm because the MLlib's NaiveBayesModel doesn't contain RDD.
+class NaiveBayesAlgorithm(val ap: AlgorithmParams)
+  extends P2LAlgorithm[PreparedData, NaiveBayesModel, Query, PredictedResult] {
+
+  @transient lazy val logger = Logger[this.type]
+
+  def train(sc: SparkContext, data: PreparedData): NaiveBayesModel = {
+    // MLLib NaiveBayes cannot handle empty training data.
+    require(data.labeledPoints.take(1).nonEmpty,
+      s"RDD[labeledPoints] in PreparedData cannot be empty." +
+      " Please check if DataSource generates TrainingData" +
+      " and Preparator generates PreparedData correctly.")
+
+    NaiveBayes.train(data.labeledPoints, ap.lambda)
+  }
+
+  def predict(model: NaiveBayesModel, query: Query): PredictedResult = {
+    val label = model.predict(Vectors.dense(
+      Array(query.attr0, query.attr1, query.attr2)
+    ))
+    new PredictedResult(label)
+  }
+
+}
+*/
